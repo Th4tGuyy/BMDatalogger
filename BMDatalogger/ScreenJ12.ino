811c4614f1212a55c0660d2506f2a909a48c31bc
BM Devs <bouletmarc@hotmail.com>
2017-04-07 02:58:35 -0400

    Add files via upload

---
+++ BMDatalogger/ScreenJ12.ino
@@ -0,0 +1,16 @@
+void SetJ12Screen() {
+    lcd.setCursor(0,0);
+    lcd.print("                ");
+    lcd.setCursor(0,1);
+    lcd.print("    NOT CONNECTED");
+    lcd.setCursor(0,2);
+    lcd.print("   IS J12 INPLACE?");
+    lcd.setCursor(0,3);
+    lcd.print("                ");
+
+    //Get States
+    EcuConnected = GetJ12Cut();
+    GetButtonTopState();
+    GetButtonBottomState();
+}
+
